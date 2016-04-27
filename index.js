1db91f5b636764ec5b34462f7a8f53ceb7744d36
ch3rr17 <cherr.batac@gmail.com>
2016-04-27 09:15:29 -0700

    2nd commit
    Fixed CSS and added in comments. Will add on ToDoList upon third commit.

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 var myApp = angular.module('myApp',[]);
-myApp.controller('DemoController', ['$scope', '$http', function($scope,$http){ //$scope object to bind stuff2nd element is a func of an array
+myApp.controller('DemoController', ['$scope', '$http', function($scope,$http){ 
 	
-	activate(); //data available when the page loads
+	activate(); //verifies data is available when the page loads
 	function activate(){
 		$http({
-		  method: 'GET', //get something..
+		  method: 'GET', 
 		  url: 'topspots.json'
-			}).then(function (response) { //...and do something with it
-		    	//debugger;
-		    	$scope.name = response.data;
+			}).then(function (response) { 
+		    	$scope.spot = response.data; //Scopes(grabs) data
 		  });
 
 	}//end of function	
